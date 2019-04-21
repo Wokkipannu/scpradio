21d66d25f916a63250fffa241208e7336f9f8ec5
Wokkipannu <iiro@kooditaikuri.fi>
2019-04-21 15:45:41 +0300

    Update scp.js

--- scp.js
+++ scp.js
@@ -1,6 +1,6 @@
 const Radio = require('./radio');
 const Logger = require('./logger');
-const { TOKEN, DEVTOKEN } = require('./config');
+const { TOKEN } = require('./config');
 
 const logger = new Logger({
   format: 'DD.MM.YYYY HH.mm.ss'
@@ -16,4 +16,4 @@ client
   .on('message', msg => logger.log(`${msg.author.username}: ${msg.content}`))
   .on('command', (msg, cmd) => logger.log(`${msg.author.username} ran command ${cmd.name}`));
 
-client.login(DEVTOKEN);
+client.login(TOKEN);
