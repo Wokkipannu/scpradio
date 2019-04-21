e6e9997e30d842b657d148a737d2a77986fbc0c2
Wokkipannu <iiro@kooditaikuri.fi>
2019-04-21 13:16:26 +0300

    Initial commit

---
+++ radio.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const path = require('path');
+
+class Radio extends Discord.Client {
+  constructor(options = {}) {
+    super();
+    this.prefix = options.prefix || '!';
+    this.logger = options.logger;
+
+    //this.commands = [];
+    this.commands = new Map();
+    this.commandsPath = options.commandsPath || 'commands';
+    this.registerCommands();
+
+    this.on('message', message => this.handleMessage(message));
+  }
+
+  registerCommands() {
+    let that = this;
+    require('require-all')({
+      dirname: path.join(__dirname, 'commands'),
+      resolve: function(Command) {
+        that.commands.set(Command.name, new Command(that));
+        //that.commands.push(new Command(that));
+      }
+    })
+  }
+
+  handleMessage(msg) {
+    try {
+      if (msg.content.startsWith(this.prefix)) {
+        const command = this.commands.get(this.capitalize(msg.content.split(this.prefix)[1]) + 'Command');
+        if (command) return command.run(msg);
+      }
+    }
+    catch(error) {
+      return console.error(error);
+    }
+  }
+
+  capitalize(word) {
+    if (typeof word !== 'string') return '';
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+}
+
+module.exports = Radio;
