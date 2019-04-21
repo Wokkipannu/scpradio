f68526ef5901bb40ba9d03514198e9129abe013a
Wokkipannu <iiro@kooditaikuri.fi>
2019-04-21 15:44:41 +0300

    Emit command run

--- scp.js
+++ scp.js
@@ -1,6 +1,6 @@
 const Radio = require('./radio');
 const Logger = require('./logger');
-const { TOKEN } = require('./config');
+const { TOKEN, DEVTOKEN } = require('./config');
 
 const logger = new Logger({
   format: 'DD.MM.YYYY HH.mm.ss'
@@ -13,6 +13,7 @@ const client = new Radio({
 
 client
   .on('ready', () => logger.log('Ready!'))
-  .on('message', msg => logger.log(`${msg.author.username}: ${msg.content}`));
+  .on('message', msg => logger.log(`${msg.author.username}: ${msg.content}`))
+  .on('command', (msg, cmd) => logger.log(`${msg.author.username} ran command ${cmd.name}`));
 
-client.login(TOKEN);
+client.login(DEVTOKEN);
