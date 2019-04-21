f3b34def3c8a2bfecb8d8ef5b38fc6930cf2af5f
Wokkipannu <iiro@kooditaikuri.fi>
2019-04-21 13:16:14 +0300

    Use new radio class

--- scp.js
+++ scp.js
@@ -1,131 +1,18 @@
-const Discord = require('discord.js');
+const Radio = require('./radio');
+const Logger = require('./logger');
+const { TOKEN } = require('./config');
 
-const client = new Discord.Client();
-const { TOKEN, PREFIX } = require('./config')
+const logger = new Logger({
+  format: 'DD.MM.YYYY HH.mm.ss'
+});
+
+const client = new Radio({
+  prefix: '::',
+  logger: logger
+});
+
+client
+  .on('ready', () => logger.log('Ready!'))
+  .on('message', msg => logger.log(`${msg.author.username}: ${msg.content}`));
 
 client.login(TOKEN);
-
-// Queue in which order to play
-const soundsQueue = [
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio1.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio2.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio3.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio4.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio5.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio6.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio7.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio0.ogg",
-  "sounds/scpradio8.ogg"
-];
-
-// Connections map
-let connections = new Map();
-
-client.on('ready', () => {
-  console.log('Radio has entered the facility');
-});
-
-// When we recieve a message
-client.on('message', msg => {
-  // If it is a command
-  if (msg.content.startsWith(PREFIX)) {
-    // If command is to join
-    if (msg.content === `${PREFIX}join`) {
-      // Check if user is in a voice channel
-      if (msg.member.voiceChannel) {
-        if (connections.get(msg.guild.id)) return msg.reply('Radio is already playing in a channel. Use `.scp leave` first and then use `.scp join` to make me join a new channel.');
-        // Connect to voice channel
-        msg.member.voiceChannel.join()
-          .then(connection => {
-            connections.set(msg.guild.id, { channel: connection, current: 0 }); // Set required values for guild
-            play(msg.guild.id); // Start playing
-          });
-      }
-      else {
-        // Tell user to join a channel
-        msg.reply('Tune to a channel to listen');
-      }
-    }
-    // If command is to leave
-    if (msg.content === `${PREFIX}leave`) {
-      let connection = connections.get(msg.guild.id); // Get the existing connection
-      if (!connection) return msg.reply('Radio is not connected'); // If connection does not exist
-      connection.channel.channel.leave(); // Leave the channel
-      connections.delete(msg.guild.id); // Delete map
-      msg.reply('Radio has left the facility');
-    }
-  }
-});
-
-async function play(guild) {
-  try {
-    // Get the connection
-    let connection = connections.get(guild);
-    // If connection does not exist
-    if (!connection) {
-      connections.delete(guild); // Delete connection
-      return console.error('Tried to play without a connection, deleting connection'); // Return error
-    }
-
-    // Try playing
-    try {
-      // Create dispatcher
-      const dispatcher = connection.channel.playFile(soundsQueue[connection.current], { volume: 0.25 })
-        .on('end', reason => {
-          console.log('Stream ended', reason);
-          connection.current = connection.current + 1; // Shift to next sound in queue
-          if (connection.current > soundsQueue.length - 1) connection.current = 0; // If current is higher than queue lenght, reset it to 0
-          console.log(`Playing ${soundsQueue[connection.current]} in guild ${guild}`);
-          play(guild); // Play again
-        })
-        .on('error', error => {
-          console.error('Dispatcher error', error); // Console log the error
-        });
-    }
-    catch(error) {
-      console.error('Play function error', error); // Console log the error
-    }
-  }
-  catch(error) {
-    console.error('Failed to get connection', error);
-  }
-}
