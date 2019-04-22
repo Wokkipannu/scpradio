1ad17fe4716dcd92c7470995445f19f965793941
Wokkipannu <iiro@kooditaikuri.fi>
2019-04-22 11:32:33 +0300

    Help command

---
+++ commands/help.js
@@ -0,0 +1,21 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = class HelpCommand {
+  constructor(client) {
+    this.client = client;
+    this.name = 'help';
+    this.description = 'Display list of commands';
+  }
+
+  run(msg) {
+    let embed = new RichEmbed()
+      .setColor('3bff00')
+      .setTitle('Commands');
+
+    this.client.commands.forEach(command => {
+      embed.addField(command.name, command.description, false);
+    });
+
+    return msg.reply(embed);
+  }
+}
