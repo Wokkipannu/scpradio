7c52eb45d2c37bf661d4f966a0f5dda4af18609b
Wokkipannu <iiro@kooditaikuri.fi>
2019-04-22 11:32:19 +0300

    Load command

---
+++ commands/load.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = class LoadCommand {
+  constructor(client) {
+    this.client = client;
+    this.name = 'load';
+    this.description = 'Load a new command';
+  }
+
+  run(msg, args) {
+    if (args) {
+      try {
+        const Command = require(path.join(__dirname, args[0]));
+        const command = new Command(this.client);
+        const name = this.client.capitalize(command.name) + 'Command';
+        if (this.client.commands.get(name)) return msg.reply('Command is already loaded');
+        this.client.commands.set(command.name, command);
+        return msg.reply(`Command ${command.name} loaded successfully`);
+      }
+      catch(error) {
+        msg.reply('Command load failed');
+        return console.error('Could not load a command', error);
+      }
+    }
+    else {
+      return msg.reply(`**Usage:** ${this.client.prefix}load filename`);
+    }
+  }
+}
